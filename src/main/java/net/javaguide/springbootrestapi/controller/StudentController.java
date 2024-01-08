b38f31cf1d1e694936cfa4e17bd7d5a24195aea4
marvelmikel <marvelmikel1997@gmail.com>
2024-01-08 01:31:09 +0000

    list student get endpoint

--- src/main/java/net/javaguide/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguide/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguide.springbootrestapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -19,5 +22,18 @@ public class StudentController {
         );
 
         return student;
+    }
+
+    // http://localhost:8080/list-students
+
+    @GetMapping("list-students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Rmesh", "Fadaere"));
+        students.add(new Student(2, "Rmesh", "Fadaere"));
+        students.add(new Student(3, "Rmesh", "Fadaere"));
+        students.add(new Student(4, "Rmesh", "Fadaere"));
+        return students;
+
     }
 }
