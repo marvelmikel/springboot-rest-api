4285bff64c5de55f5445f1c4c4490a6e5311f277
marvelmikel <marvelmikel1997@gmail.com>
2024-01-08 02:58:48 +0000

    use requestentity method to return response

--- src/main/java/net/javaguide/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguide/springbootrestapi/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.javaguide.springbootrestapi.controller;
 
 import net.javaguide.springbootrestapi.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,53 +15,64 @@ public class StudentController {
    // http://localhost:8080/student
     @GetMapping("student")
 
-    public Student getStudent(){
-
-        return new Student(
+    public ResponseEntity<Student> getStudent(){
+        Student student = new Student (
                 1,
                 "Ramesh",
                  "Fadatar"
         );
+       // return new ResponseEntity<>(student, HttpStatus.OK);
+        return ResponseEntity.ok().header("customer-header", "ejeh")
+                .body(student);
     }
 
     // http://localhost:8080/list-students
 
     @GetMapping("list-students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    public ResponseEntity <List <Student>> getStudents(){
+        List<Student>students = new ArrayList<>();
         students.add(new Student(1, "Rmesh", "Fadaere"));
         students.add(new Student(2, "Rmesh", "Fadaere"));
         students.add(new Student(3, "Rmesh", "Fadaere"));
         students.add(new Student(4, "Rmesh", "Fadaere"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     // Sprint Boot REST API WITH Path Variable
     // {id} - URI Template variable
     // http://localhost:8080/students/1
 
-    @GetMapping("students/{id}")
-    public Student studentPathVariable(@PathVariable("id") int studentId){
-        return new Student (studentId, "remesh", "fadhere");
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public ResponseEntity<Student> studentPathVariable
+            (@PathVariable("id") int studentId,
+             @PathVariable("first-name") String firstName,
+             @PathVariable("last-name") String lastName){
+        Student student = new Student(studentId, firstName, lastName);
+        return ResponseEntity.ok(student);
     }
+
 
     // Sprint Boot REST API WITH Request Param aka Query parameter
     // http://localhost:8080/students/query?id=1
     @GetMapping("students/query")
-    public Student studentRequestVariable(int id){
-        return new Student(id, "ramesh", "fatheder");
+    public ResponseEntity <Student> studentRequestVariable(
+            @RequestParam int id,
+            @RequestParam String firstName,
+            @RequestParam String lastName){
+       Student student = new Student(id, firstName, lastName);
+       return ResponseEntity.ok(student);
     }
 
     // Sprint Boot REST API that handles HTTP POST Request -creating new resource
     // @PostMapping and @requestBody
 
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody  Student student){
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity <Student> createStudent(@RequestBody  Student student){
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     // Sprint Boot REST API that handles HTTP PUT Request -updating existing resource
@@ -68,10 +80,11 @@ public class StudentController {
 
     @PutMapping("students/{id}/update")
     @ResponseStatus(HttpStatus.CREATED)
-    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
+    public ResponseEntity <Student>
+    updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return ResponseEntity.ok(student);
 
     }
 
@@ -79,10 +92,9 @@ public class StudentController {
     // @DeleteMapping and @requestBody
 
     @DeleteMapping("students/{id}/delete")
-    public String deleteStudent( @PathVariable("id") int studentId){
+    public ResponseEntity <String> deleteStudent( @PathVariable("id") int studentId){
         System.out.println(studentId);
-        return "Student Deleted Successfully!";
-
+        return ResponseEntity.ok("Student Deleted Successfully!");
     }
 
 
